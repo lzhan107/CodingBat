5f5c998af85b63e7fb8b5947541a4f600307bc39
Lzhan107 <Lzhan107@gmail.com>
2014-08-03 21:04:21 -0700

    Initial commit

--- array3/CountClumps.java
+++ array3/CountClumps.java
@@ -1,15 +1,33 @@
 package array3;
 
 public class CountClumps {
-	public static int countClumps(int[] nums){
+	public static int countClumps(int[] nums) {
 		int num = 0;
-		
-		for (int i = 0; i < nums.length; i++){
-//			if (nums[i] == 2)
+		boolean end = false;
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[j] == nums[i]) {
+					if (j - i > 1) {
+						if (j == nums.length - 1) {
+							end = true;
+							break;
+						}
+						continue;
+					}
+					num++;
+				} else {
+					i = j - 1;
+					break;
+				}
+			}
+			if (end)
+				break;
 		}
-		
 		return num;
 	}
-	
-	
+
+	public static void main(String[] args) {
+		int[] a = { 1, 1, 2, 1, 1 };
+		System.out.println(countClumps(a));
+	}
 }
